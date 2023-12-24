e073097bfae6066800c84582a55d05b64e8577f3
Mahbub Anam <mahbubanamrhythm@gmail.com>
2023-12-24 15:58:48 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,11 +1,47 @@
-const http = require('http');
+// Create web server
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var Comment = require('../models/Comment.js');
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, world!');
+// GET all comments
+router.get('/', function(req, res, next) {
+  Comment.find(function (err, comments) {
+    if (err) return next(err);
+    res.json(comments);
+  });
 });
 
-server.listen(3000, 'localhost', () => {
-    console.log('Server running at http://localhost:3000/');
+// GET single comment by ID
+router.get('/:id', function(req, res, next) {
+  Comment.findById(req.params.id, function (err, comment) {
+    if (err) return next(err);
+    res.json(comment);
+  });
 });
+
+// POST new comment
+router.post('/', function(req, res, next) {
+  Comment.create(req.body, function (err, comment) {
+    if (err) return next(err);
+    res.json(comment);
+  });
+});
+
+// PUT single comment
+router.put('/:id', function(req, res, next) {
+  Comment.findByIdAndUpdate(req.params.id, req.body, function (err, comment) {
+    if (err) return next(err);
+    res.json(comment);
+  });
+});
+
+// DELETE single comment
+router.delete('/:id', function(req, res, next) {
+  Comment.findByIdAndRemove(req.params.id, req.body, function (err, comment) {
+    if (err) return next(err);
+    res.json(comment);
+  });
+});
+
+module.exports = router;
